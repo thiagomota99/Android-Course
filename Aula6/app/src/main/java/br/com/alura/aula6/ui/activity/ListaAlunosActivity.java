5a2fd780eb613ace4152438a6fde70c212f56929
Thiago Mota <thiagomota99@outlook.com>
2020-12-04 20:38:17 -0300

    Projeto Final
    Fatoração e entendo o ciclo de vida do android framework.

--- Aula6/app/src/main/java/br/com/alura/aula6/ui/activity/ListaAlunosActivity.java
+++ Aula6/app/src/main/java/br/com/alura/aula6/ui/activity/ListaAlunosActivity.java
@@ -1,7 +1,10 @@
 package br.com.alura.aula6.ui.activity;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,15 +13,52 @@ import br.com.alura.aula6.dao.AlunoDAO;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Lista de Alunos";
+    private AlunoDAO alunoDAO = new AlunoDAO();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        setTitle("Lista de Alunos");
-        AlunoDAO alunoDAO = new AlunoDAO();
-        ListView listView = findViewById(R.id.activity_lista_de_alunos_listview);
-        listView.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,alunoDAO.todos()));
+        //Alterando nome da App bar
+        setTitle(TITULO_APPBAR);
+        configuraFabNovoAluno();
 
+
+    }
+
+    private void configuraFabNovoAluno() {
+        //Pegando referência da View FloatingActionButton
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_novo_aluno_fab);
+
+        //Setando o método de onclick do listener
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Chamando nova activity
+                abriFormularioAlunoActivity();
+            }
+        });
+    }
+
+    private void abriFormularioAlunoActivity() {
+        startActivity(new Intent(this,NovoAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Criando instância do dao.
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        //Pegando a referência da View ListView
+        ListView listView = findViewById(R.id.activity_lista_de_alunos_listview);
+
+        //setando o adpater da view para exibição dos dados na lista.
+        listView.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,alunoDAO.todos()));
     }
 }
--- Aula6/app/src/main/java/br/com/alura/aula6/ui/activity/NovoAlunoActivity.java
+++ Aula6/app/src/main/java/br/com/alura/aula6/ui/activity/NovoAlunoActivity.java
@@ -1,6 +1,7 @@
 package br.com.alura.aula6.ui.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,31 +15,56 @@ import br.com.alura.aula6.model.Aluno;
 
 public class NovoAlunoActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Novo Aluno";
+    private EditText campoNome;
+    private EditText campoTelefone;
+    private EditText campoEmail;
+    final AlunoDAO alunoDAO = new AlunoDAO();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_aluno);
+        setTitle(TITULO_APPBAR);
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
+    }
 
-        final AlunoDAO alunoDAO = new AlunoDAO();
 
-        final EditText campoNome = findViewById(R.id.activity_novo_aluno_campo_nome);
-        final EditText campoTelefone = findViewById(R.id.activity_novo_aluno_campo_telefone);
-        final EditText campoEmail = findViewById(R.id.activity_novo_aluno_campo_email);
-
+    //Configuração do botão Salvar
+    private void configuraBotaoSalvar() {
         Button btnSalvar = findViewById(R.id.activity_novo_aluno_botao_salvar);
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nome = campoNome.getText().toString();
-                String telefone = campoTelefone.getText().toString();
-                String email = campoEmail.getText().toString();
-
-                Aluno alunoCriado = new Aluno(nome,telefone,email);
-                alunoDAO.salvar(alunoCriado);
-
-                //Inializnado outra activity
-                startActivity(new Intent(NovoAlunoActivity.this, ListaAlunosActivity.class));
+                Aluno alunoCriado = criaAluno();
+                salva(alunoCriado);
             }
         });
+    }
+
+    //Inicializr atributos
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_novo_aluno_campo_nome);
+        campoTelefone = findViewById(R.id.activity_novo_aluno_campo_telefone);
+        campoEmail = findViewById(R.id.activity_novo_aluno_campo_email);
+    }
+
+    //Salvar aluno
+    private void salva(Aluno aluno) {
+        alunoDAO.salvar(aluno);
+
+        //Destruir activity
+        finish();
+    }
+
+    //Criando aluno
+    @NonNull
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        return new Aluno(nome,telefone,email);
     }
 }
